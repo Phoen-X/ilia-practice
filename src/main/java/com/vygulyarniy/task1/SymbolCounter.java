b5fc2b5cff2a7d6743e3b7e2e068415610fee0d2
trytosolve <trytosolve@gmail.com>
2017-07-17 17:44:12 +0300

    commit

--- src/main/java/com/vygulyarniy/task1/SymbolCounter.java
+++ src/main/java/com/vygulyarniy/task1/SymbolCounter.java
@@ -1,15 +1,32 @@
 package com.vygulyarniy.task1;
 
+import jdk.nashorn.internal.ir.Symbol;
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+import java.util.ArrayList;
 
 public class SymbolCounter {
     /**
      * Нужно найти самую популярную букву в тексте и вернуть статистику ее использования.
      * Пример: для текста "Мы шли, шли, шли, пирожок нашли." нужно вернуть букву "и", и кол-вом использований 5
+     *
      * @param text текст для подсчетов
      * @return информация о самой часто-упоминаемой букве/цифре
      */
     public SymbolStatistics findMostPopularSymbol(String text) {
-        throw new NotImplementedException();
+        char[] charArray = text.toCharArray();
+        ArrayList<SymbolStatistics> statisticsArrayList = new ArrayList<SymbolStatistics>();
+        for (int i = 0; i < text.length(); i++) {
+            for (int j = 0; j < text.length(); j++) {
+                int n = 0;
+                if (charArray[i] == charArray[j]) {
+                    n++;
+                }
+                statisticsArrayList.add(new SymbolStatistics(charArray[i], n));
+            }
+        }
+
+        SymbolStatistics result = new SymbolStatistics(symbol, count);
+        return result;
     }
 }
