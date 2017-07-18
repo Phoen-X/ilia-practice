899e5287926c3ac9ce178f61afe78bac7947d1fe
trytosolve <trytosolve@gmail.com>
2017-07-18 11:21:31 +0300

    commit

--- src/main/java/com/vygulyarniy/task2/DigitsCounter.java
+++ src/main/java/com/vygulyarniy/task2/DigitsCounter.java
@@ -11,25 +11,29 @@ public class DigitsCounter {
      * @param text текст, в котором мы считаем цифры
      * @return кол-во цифр в приведенном тексте
      */
-    int countIntDigits=0;
+    int countIntDigits = 0;
 
 
-    char[] numArray = {'0','1','2','3','4','5','6','7','8','9'};
+    char[] numArray = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+
     public String digitsCount(String text) {
-        char[] charArray = text.toCharArray();
-        if (text==null || text.equals("")){
+        if (text == null || text.equals("")) {
             return "0";
-        }else
-        for (int i=0; i<text.length();i++){
-            for (int j=0; j<numArray.length;j++){
-                if(charArray[i]==numArray[j]){
-                    countIntDigits++;
+        } else {
+            char[] charArray = text.toCharArray();
+            for (int i = 0; i < text.length(); i++) {
+                for (int j = 0; j < numArray.length; j++) {
+                    if (charArray[i] == numArray[j]) {
+                        countIntDigits++;
+                    }
                 }
             }
+            String countStringDigits = Integer.toString(countIntDigits);
+            return countStringDigits;
         }
-        String countStringDigits = Integer.toString(countIntDigits);
-        return countStringDigits;
     }
 
-    public String digitsCountAllNum(){throw new NotImplementedException();}
+    public String digitsCountAllNum() {
+        throw new NotImplementedException();
+    }
 }
