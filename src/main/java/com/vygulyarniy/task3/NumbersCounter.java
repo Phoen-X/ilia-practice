fb233b1f1364ea175782a23910610ab32131de30
trytosolve <trytosolve@gmail.com>
2017-07-18 12:31:15 +0300

    commit

--- src/main/java/com/vygulyarniy/task3/NumbersCounter.java
+++ src/main/java/com/vygulyarniy/task3/NumbersCounter.java
@@ -1,5 +1,7 @@
 package com.vygulyarniy.task3;
 
+import java.util.regex.*;
+
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 public class NumbersCounter {
@@ -8,10 +10,23 @@ public class NumbersCounter {
      * При этом дробные числа тоже должны распознаваться, например 17.25 это одно число.
      * Пример: в тексте "мы выехали в 15:25 из дому, проехали 700 км и потратили 95.5 баксов. Это были прекрасные 2 дня" 5 чисел.
      * (время здесь не является числом, поэтому там 2 числа - часы и минуты отдельно)
+     *
      * @param text текст для подсчета
      * @return кол-во чисел
      */
     public int countNumbers(String text) {
-        throw new NotImplementedException();
+
+
+        Pattern pat = Pattern.compile("[-]?[0-9]+(.[0-9]+)?");
+        Matcher matcher = pat.matcher(text);
+        while (matcher.find()) {
+            System.out.println(matcher.group());
+        }
+        return 1;
+    }
+
+    public static void main(String[] args) {
+        NumbersCounter nc = new NumbersCounter();
+        nc.countNumbers("Text246s 54895 45 45 41 151");
     }
 }
