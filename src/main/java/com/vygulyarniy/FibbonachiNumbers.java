5ad476ec9de9d4c099e6d4f81c86e688a0cda2ed
trytosolve <trytosolve@gmail.com>
2017-07-27 15:26:14 +0300

    11

--- src/main/java/com/vygulyarniy/FibbonachiNumbers.java
+++ src/main/java/com/vygulyarniy/FibbonachiNumbers.java
@@ -10,21 +10,38 @@ public class FibbonachiNumbers {
      * @param index порядковый номер числа, начиная с 1
      * @return
      */
+
     public int giveNumber(int index) {
+        int result = 0;
         if (index <= 0) {
             return Integer.parseInt(null);
         }
         if (index < 3) {
             return 1;
-        } else {
-            int[] fibo = new int[index];
-            fibo[0]=1;
-            fibo[1]=1;
-            for (int i = 2; i < index; i++) {
-                fibo[i]=fibo[i-1]+fibo[i-2];
-            }
-            return fibo[index - 1];
         }
+        for (int i = 2; i < index; i++) {
+            result = giveNumber(index - 1) + giveNumber(index - 2);
+        }
+        return result;
     }
-
 }
+
+
+
+//    public int giveNumber(int index) {
+//        if (index <= 0) {
+//            return Integer.parseInt(null);
+//        }
+//        if (index < 3) {
+//            return 1;
+//        } else {
+//            int[] fibo = new int[index];
+//            fibo[0]=1;
+//            fibo[1]=1;
+//            for (int i = 2; i < index; i++) {
+//                fibo[i]=fibo[i-1]+fibo[i-2];
+//            }
+//            return fibo[index - 1];
+//        }
+//    }
+
