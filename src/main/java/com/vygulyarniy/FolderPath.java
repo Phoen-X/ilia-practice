28751477ff1b6b99b1eef6e663c8c45611b83904
trytosolve <trytosolve@gmail.com>
2017-07-31 11:45:40 +0300

    commit

---
+++ src/main/java/com/vygulyarniy/FolderPath.java
@@ -0,0 +1,45 @@
+package com.vygulyarniy;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.net.Socket;
+import java.net.SocketPermission;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by IRedko on 31.07.2017.
+ */
+public class FolderPath {
+
+    public static void main(String[] args) {
+        FolderPath folderPath = new FolderPath();
+        folderPath.checkFolder("D:\\b");
+    }
+
+    public void checkFolder(String testPath) {
+        File path = new File(testPath);
+        File[] files = path.listFiles();
+        List<String> filesInDirectory = new ArrayList<>();
+        List<String> folderInDirectory = new ArrayList<>();
+        if (files == null) {
+            System.out.println("Данная директория пуста");
+        } else {
+            for (File file : files) {
+                if (file.isDirectory()) {
+                    folderInDirectory.add(file.getName());
+                    checkFolder(file.getAbsolutePath());
+                }
+                if (file.isFile()) {
+                    filesInDirectory.add(file.getName());
+                }
+            }
+        }
+        System.out.println("Файлы в директории "+ testPath+" :"+filesInDirectory );
+        System.out.println("Папки в директории "+ testPath+" :" +folderInDirectory );
+    }
+}
+
+
+
+
