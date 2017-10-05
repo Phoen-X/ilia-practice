73a3bf230be8ef7333fc9a627b10223052a8d4ae
trytosolve <trytosolve@gmail.com>
2017-10-05 15:33:50 +0300

    recommit

---
+++ src/main/java/com/iredko/testBook/ChangeSystemOut.java
@@ -0,0 +1,10 @@
+package com.iredko.testBook;
+
+import java.io.PrintWriter;
+
+public class ChangeSystemOut {
+    public static void main(String[] args) {
+        PrintWriter out = new PrintWriter(System.out, true);
+        out.println("Hello, word");
+    }
+}
